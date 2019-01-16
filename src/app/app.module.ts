a82d628e58fb0aed5d45eb49714f93e8c969692a
aeser9o1 <aashiskumar986@gmail.com>
2019-01-16 13:29:39 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './home/landing/landing.component';
+import { AboutMeComponent } from './home/about-me/about-me.component';
+import { SkillsComponent } from './home/skills/skills.component';
+import { ExperienceComponent } from './home/experience/experience.component';
+import { ProjectsComponent } from './home/projects/projects.component';
+import { AcheivementsComponent } from './home/acheivements/acheivements.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    AcheivementsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
