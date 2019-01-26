da9e62e32c8983690cd4c45f014c3228acec226b
aeser9o1 <aashiskumar986@gmail.com>
2019-01-26 19:30:01 +0530

    completed resume

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup,FormBuilder,Validators,NgForm} from '@angular/forms';
+import { AngularFireDatabase } from '@angular/fire/database';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder, private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.form=this.fb.group({
+      email:['',[
+        Validators.required,
+        Validators.email
+      ]],
+      message:['', Validators.required]
+    })
+  }
+  submitForm(){
+    this.db.list('messages').push(this.form.value);
+    alert('your message has been recorded')
   }
 
 }
